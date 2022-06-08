aaee43603d065bc0b7c7b1c25198f0959586dbd0
badubko <badubko@gmail.com>
2022-06-08 14:50:48 -0300

    Primer commit de la version

---
+++ deploy_back_samba_V1.1.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# Version 1.1
+# Deploy script for back_samba
+# 
+
+# Should be run as root or sudo
+# Check to be added later...
+# See you later, aligator...
+
+TARGET_SCRIPT="./back_samba_V1.2.sh"
+DEST_DIR="/opt/back_samba/"
+
+cp --update --verbose ${TARGET_SCRIPT} ${DEST_DIR}
+
+chmod 755 ${DEST_DIR}$(basename ${TARGET_SCRIPT})
