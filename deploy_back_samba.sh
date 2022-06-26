ce0ad07d8eb6e0ceb60d18e3bfed2fdb1368ac12
badubko <badubko@gmail.com>
2022-06-25 23:43:56 -0300

    Modificado el deploy
    para que obtenga el nombre del exclude file del back_samba.sh

--- deploy_back_samba.sh
+++ deploy_back_samba.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
-# Version 2.6
-# Deploy script for back_samba
 # 
-VERSION="2.6"
+# Deploy script for back_samba
+# Version 2.6
+# Version 2.8 Gets exclude file name from back_samba.sh
+
+VERSION="2.8"
 
 # Should be run as root or sudo
 # Check to be added later...
-# See you later, aligator...
-# It would be nice to get the exclude... file name from the back_samba.sh
-# wouldla & shouldla....
 
 TARGET_SCRIPT="./back_samba.sh"
-TARGET_EXCLUDE="./exclude_patterns_file.txt"
+
+TARGET_EXCLUDE="./"$(grep "EXCLUDE_FILE_NAME=" ./back_samba.sh| sed -r 's/EXCLUDE_FILE_NAME=\"(.*)\"/\1/' )
 
 DEST_DIR="/opt/back_samba/"
 
