764a07a5a2ee91a3bf099d27c539c16327521998
badubko <badubko@gmail.com>
2022-07-09 19:32:20 -0300

    Adaptaciones BASE para la version 4.0

--- back_samba/deploy_back_samba.sh
+++ back_samba/deploy_back_samba.sh
@@ -3,8 +3,10 @@
 # Deploy script for back_samba
 # Version 2.6
 # Version 2.8 Gets exclude file name from back_samba.sh
+# Version 4.0 Adapted for generic script which generates specific script
+#			  Should be improved (no harcoded names...)
 
-VERSION="2.8"
+VERSION="4.0"
 
 # Should be run as root or sudo
 # Check to be added later...
@@ -14,7 +16,7 @@ TARGET_SCRIPT="./back_samba.sh"
 # Gets exclude file name from back_samba.sh. It's assumed that it resides in the same directory
 # as the main script.
 
-TARGET_EXCLUDE="./"$(grep "EXCLUDE_FILE_NAME=" ./back_samba.sh| sed -r 's/EXCLUDE_FILE_NAME=\"(.*)\"/\1/' )
+TARGET_EXCLUDE="./"$(grep "EXCLUDE_FILE_NAME_CFG=" ./back_samba.cfg| sed -r 's/EXCLUDE_FILE_NAME_CFG=\"(.*)\"/\1/' )
 
 DEST_DIR="/opt/back_samba/"
 
