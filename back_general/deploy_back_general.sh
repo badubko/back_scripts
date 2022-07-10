851bfdedc8821e5debf59564891246bead2504b5
badubko <badubko@gmail.com>
2022-07-10 19:09:44 -0300

    Implementada solucion transitoria. Ver comentario

--- back_general/deploy_back_general.sh
+++ back_general/deploy_back_general.sh
@@ -49,6 +49,10 @@ DEPLOY_DESTINATION_DIR=${DEPLOY_DESTINATION_DIR_CFG}
 
 EXCLUDE_FILE_NAME=${EXCLUDE_FILE_NAME_CFG}
 
-echo cp --update --verbose ${TGT_SCRIPT_NAME} ${EXCLUDE_FILE_NAME} ${DEPLOY_DESTINATION_DIR}
+# Solucion transitoria para evitar que se pisen los archivos en caso de que no
+# exista el deploy destination dir
+
+echo cp --update --verbose ${TGT_SCRIPT_NAME}   ${DEPLOY_DESTINATION_DIR}
+echo cp --update --verbose ${EXCLUDE_FILE_NAME} ${DEPLOY_DESTINATION_DIR}
 
 echo chmod 755 ${DEPLOY_DESTINATION_DIR}$(basename ${TGT_SCRIPT_NAME})
