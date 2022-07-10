fa8e5e4b1922a1ba680a7e7e4d35035a815e3068
badubko <badubko@gmail.com>
2022-07-10 18:42:42 -0300

    Xformer mejorado

--- back_general/xformer.sh
+++ back_general/xformer.sh
@@ -1,23 +1,43 @@
 #! /bin/bash
+# xformer == trans-former
+# 
+# Version 5.0 Genera a partir del script generico, un script de backup especifico, 
+#             tomando los parametros del config file pasado como argumento.
+#
+VERSION=5.0
 
-VERSION=4.0
+CONFIG_FILE_NAME=${1}
 
 if [ $# -eq 0 ]
 then 
- echo -e "No se indico nombre del config file...\n"
+ echo -e "No se indico nombre del config file..."
+ echo -e "Usage: xformer.sh <config_file_name.cfg \n"
  exit
 else
- if [ ! -f ${1} ]
+ if [ "${CONFIG_FILE_NAME##*.}" != "cfg" ]
  then
-     echo -e "No existe el config file...${1} \n"
+     echo -e "El archivo no es del tipo .cfg (config file)...${CONFIG_FILE_NAME} \n"
      exit
+ else
+	 if [ ! -f ${CONFIG_FILE_NAME} ]
+	 then
+	     echo -e "No existe el config file...${CONFIG_FILE_NAME} \n"
+	     exit
+	 fi
  fi
 fi
-CONFIG_FILE_NAME=${1}
+
+# echo ${CONFIG_FILE_NAME} ; exit
+
 #-----------------------------------------------------------------------
 #   Obtener nombre del script de destino
 #-----------------------------------------------------------------------
-TGT_SCRIPT_NAME="./"$(grep "TGT_SCRIPT_NAME=" ./${CONFIG_FILE_NAME}| sed -r 's/TGT_SCRIPT_NAME=\"(.*)\"/\1/' )
+# TGT_SCRIPT_NAME="./"$(grep "TGT_SCRIPT_NAME=" ./${CONFIG_FILE_NAME}| sed -r 's/TGT_SCRIPT_NAME=\"(.*)\"/\1/' )
+
+# En vez de usar el metodo "elegante" de mas arriba, importamos directamente el config file
+# en el cual estan definidos los valores de las variables.
+
+. ./${CONFIG_FILE_NAME}
 
 echo "Target script name: " ${TGT_SCRIPT_NAME}
 
@@ -26,4 +46,4 @@ echo "Target script name: " ${TGT_SCRIPT_NAME}
 #-----------------------------------------------------------------------
 
 INS_MARKER="INSERT_CONFIG_HERE"
-sed "/${INS_MARKER}/r  ${CONFIG_FILE_NAME}" ./back_a_samba_gen.sh > ${TGT_SCRIPT_NAME}
+sed "/${INS_MARKER}/r  ${CONFIG_FILE_NAME}" ./back_generico.sh > ${TGT_SCRIPT_NAME}
