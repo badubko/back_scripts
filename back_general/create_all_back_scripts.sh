e76ab2dfa0f7f8d6de23be557dd2bb9de43555fa
badubko <badubko@gmail.com>
2022-07-10 20:25:07 -0300

    Script para recrear todos los scripts de backup

---
+++ back_general/create_all_back_scripts.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Version 5.0 2022.07.10 Va a crear todos los scripts especificos a partir del los
+#                        config files *.cfg
+
+for CONFIG_FILE in *.cfg
+do
+   bash ./xformer ${CONFIG_FILE}
+done   
